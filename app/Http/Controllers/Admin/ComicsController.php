b40caf659bf7b4158821e555af56af72ccbd5038
MassimoPeruga <spamma-mutevole0p@icloud.com>
2024-02-01 21:47:42 +0100

    feat: create resource controler

---
+++ app/Http/Controllers/Admin/ComicsController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class ComicsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $comics = Comic::all();
+
+        return view('comics.comics-table', compact('comics'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('comics.create-form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $new_comic = new Comic();
+
+        $new_comic->title = $data['title'];
+        $new_comic->description = $data['description'];
+        $new_comic->thumb = $data['thumb'];
+        $new_comic->price = $data['price'];
+        $new_comic->series = $data['series'];
+        $new_comic->sale_date = $data['sale_date'];
+        $new_comic->type = $data['type'];
+        $new_comic->artists = $data['artists'];
+        $new_comic->writers = $data['writers'];
+
+        $new_comic->save();
+        return redirect()->route('comics.show', $new_comic->id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comic $comic)
+    {
+        return view('comics.comic-details', compact('comic'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
